f5f3bb0c09b2f88e89f4f57e72af03ba2f4454b6
litzana20015 <litzana_315@hotmail.com>
2017-07-18 00:33:56 -0400

    version 1

---
+++ webhierbalife/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webhierbalife.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private Models.TiendaHerbalifeEntities bd = new Models.TiendaHerbalifeEntities();
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buscar(string id="" )
+        {
+            var productos = bd.Producto.Where(x => x.Descripcion.Contains(id)).ToList();
+            return View(productos);
+        }
+    }
+}
---
+++ webhierbalife/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webhierbalife.MvcApplication" Language="C#" %>
